2b5a3fa49fd6ee0ad9a5a43800fce947acef5b20
alexandr <fart82@ukr.net>
2020-01-03 17:22:23 +0200

    a

--- src/main/java/com/example/validator/Validator.java
+++ src/main/java/com/example/validator/Validator.java
@@ -1,39 +1,12 @@
 package com.example.validator;
 
 import com.example.entity.CustomerAccount;
-import com.example.exception.CustomerAccountException;
-import org.apache.commons.validator.routines.EmailValidator;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Validator {
+public interface Validator {
 
-    public void isNull(Long id) {
-        if (id == null) {
-            throw new CustomerAccountException("Id can not be null");
-        }
-    }
+    void isNull(Long id);
 
-    public long parseLong(String id) {
-        try {
-            return Long.parseLong(id);
-        } catch (Exception e) {
-            throw new CustomerAccountException("Id must be a digit");
-        }
-    }
+    long parseLong(String id);
 
-    public void isEmptyCustomerAccount(CustomerAccount customerAccount){
-        if (customerAccount.getName() == null){
-            throw new CustomerAccountException("Name can not be null");
-        }
-
-        if (customerAccount.getName().isEmpty()){
-            throw new CustomerAccountException("Name can not be empty");
-        }
-
-        if (!EmailValidator.getInstance().isValid(customerAccount.getEmail())){
-            throw new CustomerAccountException("Email address not valid");
-        }
-
-    }
+    void isEmptyCustomerAccount(CustomerAccount customerAccount);
 }
